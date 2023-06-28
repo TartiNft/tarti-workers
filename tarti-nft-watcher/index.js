f0e5ba4b6e5339c000b5c62d344460a03e0d3c79
Evan de la Cruz <evan.delacruz@gmail.com>
2023-06-28 13:39:09 -0700

    hard code a trim based on length to force a test to go through

--- tarti-nft-watcher/index.js
+++ tarti-nft-watcher/index.js
@@ -92,7 +92,7 @@ module.exports = async function (context, myTimer) {
         context.log("Find NFTs without metadata");
         const uncreatedMetadatas = [];
         for (let tokenId = totalSupply; tokenId > 0; tokenId--) {
-            const tokenUri = (await tokenToQueueContract.methods.tokenURI(tokenId).call()).replace(/[^\x00-\x7F]/g, "");
+            const tokenUri = (await tokenToQueueContract.methods.tokenURI(tokenId).call()).substring(0, newTokenUri.length);
             context.log(`Checking if ${tokenUri} is the same as ${newTokenUri}`)
             if (tokenUri != newTokenUri) {
                 break;
