fc64e892560c37e664bd60a73f488934fd745f4a
Evan de la Cruz <evan.delacruz@gmail.com>
2023-06-27 23:03:00 -0700

    Updated to latest accidentally had old code in here copied from other project

--- tarti-nft-watcher/index.js
+++ tarti-nft-watcher/index.js
@@ -5,78 +5,95 @@ module.exports = async function (context, myTimer) {
         context.log('JavaScript is running late!');
     }
 
-    context.log('Function is running!');
     // on dev this is in local.settings.json
     // on prod it is in Azure app config
     // on test it is in Azure deploy slot config
     const ethClientUri = process.env["ETH_CLIENT_URL"];
-    const newlyMintedTokenUri = `ipfs://${process.env['NEW_TARTIST_METADATA_CID']}`;
-    const tokenInProgressUri = `ipfs://${process.env['CREATING_TARTIST_METADATA_CID']}`;
+    const newlyMintedTartistUri = process.env["NEW_TARTIST_METADATA_CID"];
+    const newlyMintedTartiUri = process.env["NEW_TARTI_METADATA_CID"];
 
     const { Web3 } = require('web3');
     const web3 = new Web3(ethClientUri);
+    web3.eth.accounts.wallet.add(process.env['CONTRACT_OWNER_WALLET_PK']);
 
-    const getContract = async (web3) => {
+    const getContract = async (web3, contractJsonFile) => {
         const fs = require('fs');
-        const contractFileJson = JSON.parse(fs.readFileSync(__dirname + "/contracts/Tartist.json"));
+        const contractJson = JSON.parse(fs.readFileSync(contractJsonFile));
         const netId = await web3.eth.net.getId();
-        const deployedNetwork = contractFileJson.networks[netId];
+        const deployedNetwork = contractJson.networks[netId];
         return new web3.eth.Contract(
-            contractFileJson.abi,
+            contractJson.abi,
             deployedNetwork && deployedNetwork.address
         );
     };
 
-    const latestBlock = await web3.eth.getBlock('latest');;
-    const blockNumber = await web3.eth.getBlockNumber();
+    const enqueueTokenEvents = async (web3, contractJsonFile, newTokenUri, queueConnectionString, queueName) => {
 
-    const contract = await getContract(web3); //new web3.eth.Contract(contractAbiJson, tartistContractAddress);  //getContract(web3);
-    const totalSupply = parseInt(await contract.methods.totalSupply().call());
-    const queueMessages = [];
+        const tokenToQueueContract = await getContract(web3, contractJsonFile);
+        const tartistContract = await getContract(web3, __dirname + "/contracts/Tartist.json");
 
-    //go back through tokens and find the last one that has not been created yet
-    const uncreatedTartists = [];
-    for (let tokenId = totalSupply; tokenId > 0; tokenId--) {
-        const tokenUri = await contract.methods.tokenURI(tokenId).call();
-        context.log("token uri from contract: " + tokenUri);
-        if (tokenUri != newlyMintedTokenUri) {
-            break;
+        const totalSupply = parseInt(await tokenToQueueContract.methods.totalSupply().call());
+        const queueMessages = [];
+
+        //go back through tokens and find the last one that has not been created yet
+        const uncreatedMetadatas = [];
+        for (let tokenId = totalSupply; tokenId > 0; tokenId--) {
+            const tokenUri = await tokenToQueueContract.methods.tokenURI(tokenId).call();
+            if (tokenUri != newTokenUri) {
+                break;
+            }
+            queueMessages.push({ body: tokenId.toString() });
+            uncreatedMetadatas.push(tokenId);
         }
-        queueMessages.push({ body: tokenId.toString() });
-        uncreatedTartists.push(tokenId);
-    }
 
-    const serviceBusConnectionString = process.env['SERVICE_BUS_CONNECTION_STRING'];
-    const serviceBusQueueName = process.env['SERVICE_BUS_QUEUE_NAME'];
-    const { ServiceBusClient } = require("@azure/service-bus");
-    const sbClient = new ServiceBusClient(serviceBusConnectionString);
-    const sender = sbClient.createSender(serviceBusQueueName);
-    if (queueMessages.length == 0) {
-        context.log("There are no messages to queue");
-        return;
-    }
-    let batch = await sender.createMessageBatch();
-    for (let i = 0; i < queueMessages.length; i++) {
-        if (!batch.tryAddMessage(queueMessages[i])) {
-            // if it fails to add the message to the current batch
-            // send the current batch as it is full
-            await sender.sendMessages(batch);
-
-            // then, create a new batch 
-            batch = await sender.createMessageBatch();
-
-            // now, add the message failed to be added to the previous batch to this batch
+        const { ServiceBusClient } = require("@azure/service-bus");
+        const sbClient = new ServiceBusClient(queueConnectionString);
+        const sender = sbClient.createSender(queueName);
+        if (queueMessages.length == 0) {
+            context.log(`There are no messages to enqueue for ${queueName}`);
+            return;
+        }
+        let batch = await sender.createMessageBatch();
+        for (let i = 0; i < queueMessages.length; i++) {
             if (!batch.tryAddMessage(queueMessages[i])) {
-                // if it still can't be added to the batch, the individual message is probably too big to fit in a batch
-                throw new Error("Message too big to fit in a batch");
+                // if it fails to add the message to the current batch
+                // send the current batch as it is full
+                await sender.sendMessages(batch);
+
+                // then, create a new batch 
+                batch = await sender.createMessageBatch();
+
+                // now, add the message failed to be added to the previous batch to this batch
+                if (!batch.tryAddMessage(queueMessages[i])) {
+                    // if it still can't be added to the batch, the individual message is probably too big to fit in a batch
+                    throw new Error("Message too big to fit in a batch");
+                }
             }
         }
-    }
-    await sender.sendMessages(batch);
-    await sender.close();
+        await sender.sendMessages(batch);
+        await sender.close();
 
-    console.log("totalSupply: " + totalSupply)
-    console.log("latest: " + latestBlock)
-    context.log("block: " + blockNumber)
+        //lets mark them on the block chain as being in process
+        //doing syncronous for now.. better to do async and do a waitall or allsettled afterwards, i reckon
+        for (let i = 0; i < uncreatedMetadatas.length; i++) {
+            //for Tartis we are delegating via Tartists, since the Tartist contract owns the Tarti contract
+            await tartistContract.methods.setCreationStarted(uncreatedMetadatas[i], tokenToQueueContract != tartistContract).send({ from: process.env['CONTRACT_OWNER_WALLET_ADDRESS'] })
+        }
+    };
+
+    enqueueTokenEvents(
+        web3, __dirname + "/contracts/Tartist.json",
+        newlyMintedTartistUri,
+        process.env['TARTIST_QUEUE_CONNECTION_STRING'],
+        process.env['TARTIST_QUEUE_NAME']
+    );
+
+    enqueueTokenEvents(
+        web3, __dirname + "/contracts/Tarti.json",
+        newlyMintedTartiUri,
+        process.env['TARTI_QUEUE_CONNECTION_STRING'],
+        process.env['TARTI_QUEUE_NAME']
+    );
+
     context.log('JavaScript timer trigger function ran!', timeStamp);
 };
