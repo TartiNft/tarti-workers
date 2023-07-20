8f5262fcd7df8b954a63ce0bc4beb46ebb71c8b3
Evan de la Cruz <evan.delacruz@gmail.com>
2023-07-19 17:25:25 -0700

    Added code comments

--- traithttpclient.js
+++ traithttpclient.js
@@ -1,3 +1,10 @@
+/**
+ * POST something to the TRAIT AI API (via TraitHttpIO) 
+ * @param {string} command 
+ * @param {string[]} contextParams 
+ * @param {*} body 
+ * @returns An object representing the response
+ */
 const postTraitAi = (command, contextParams, body) => {
     const axios = require('axios');
     let contextParamsQueryString = "";
@@ -6,16 +13,22 @@ const postTraitAi = (command, contextParams, body) => {
         contextParamsQueryString = `&${param}=${contextParams[param]} ${contextParamsQueryString}`;
     }
     return axios.post(`${process.env["TRAIT_HTTP_URI"]}/${command}?${contextParamsQueryString.trim()}`, body).then(function (response) {
-        let parsedResponse = response.data.BotResponse.trim();
+        const parsedResponseString = response.data.BotResponse.trim();
         try {
-            parsedResponse = JSON.parse(parsedResponse);
+            const parsedResponse = JSON.parse(parsedResponseString);
+            return parsedResponse;
         } catch (error) {
             throw new Error("Invalid response from Trait AI: " + parsedResponse);
         }
-        return parsedResponse;
     });
 };
 
+/**
+ * GET something from the TRAIT AI API (via TraitHttpIO) 
+ * @param {string} command 
+ * @param {string[]} contextParams 
+ * @returns An object representing the response
+ */
 const getTraitAi = (command, contextParams) => {
     const axios = require('axios');
     let contextParamsQueryString = "";
@@ -34,6 +47,13 @@ const getTraitAi = (command, contextParams) => {
     });
 };
 
+/**
+ * Prompt a TRAIT AI bot (via TraitHttpIO)
+ * @param {string} prompt 
+ * @param {*} metaData 
+ * @param {*} contextParams 
+ * @returns An object representing the response
+ */
 const promptBot = (prompt, metaData, contextParams) => {
     if (!contextParams) {
         contextParams = {};
