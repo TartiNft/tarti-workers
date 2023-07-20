a177d255cb8ec5dba4037e9ccdf5a5ab40dcda91
Evan de la Cruz <evan.delacruz@gmail.com>
2023-07-19 17:02:59 -0700

    Minor code cleanup, add comments.

--- tarti-nft-worker/index.js
+++ tarti-nft-worker/index.js
@@ -1,117 +1,88 @@
+/**
+ * - Dequeue Tarti mint events from the service bus
+ * - Use offchain service to ask bot to create the media
+ * - Create and pin NFT metadata for Tarti to IPFS
+ * @todo check if the tarti has already been created!
+ * 
+ * @param {object} context 
+ * @param {string} tartiSbMsg 
+ */
 module.exports = async function (context, tartiSbMsg) {
+
+    //Get smart contract definitions
     const nft = require("../nft");
-    const tokenId = parseInt(tartiSbMsg);
     const tartistContract = await nft.getContract(__dirname + "/../contracts/Tartist.json");
     const tartiContract = await nft.getContract(__dirname + "/../contracts/Tarti.json");
 
-    //get info about the artist
+    //Get info about the Tartist
+    const tokenId = parseInt(tartiSbMsg);
     const tartistId = await tartiContract.methods.artCreators(tokenId).call();
     const tartistUrl = convertIpfsToWeb2GatewayUri(await tartistContract.methods.tokenURI(tartistId).call());
     const tartistMetadata = await downloadFileToMemory(tartistUrl);
-
     if (!tartistMetadata) {
         throw new Error("Cannot load Tartist to generate Tarti");
     }
-    //@todo check if the tarti has already been created!
 
-    //generate metadata with some place holders
-    const metaAttributes = [];
-    metaAttributes.push({
-        "trait_type": "birthday",
-        "display_type": "date",
-        "value": Math.floor(Date.now() / 1000)
-    });
+    //Generate beat package from MakeBeat which includes most of what we need from Trait API
+    //@todo consider breaking out the titling, videos, images etc from the actual beat making.
+    const traitio = require("../traithttpclient");
+    let retryCount = 0;
+    let beatInfo;
+    while (retryCount < 3) {
+        beatInfo = (await traitio.promptBot("MakeBeat", tartistMetadata));
+        if ((typeof beatInfo === 'object') && beatInfo["title"]) {
+            break;
+        }
+    }
+    if ((typeof beatInfo !== 'object') || !beatInfo["title"]) {
+        throw new Error("Invalid response from Trait AI when trying to make beat: " + beatInfo);
+    }
 
-    //wrting this here for reference need to move this elsewhere post-mint
-    //find others..
-    //also be cool to add urls for the track images and the tracked out wavs or the zip package
-    metaAttributes.push({
-        "trait_type": "Key",
-        "value": "C#"
-    });
-    metaAttributes.push({
-        "trait_type": "Tempo",
-        "value": 125.56
-    });
-    metaAttributes.push({
-        "trait_type": "Created by",
-        "value": "Name of the Tartist"
-    });
-    metaAttributes.push({
-        "trait_type": "Tartist Traits",
-        "value": "MusicProducer, ArpegioHats, etc"
-    });
-
-    //create metadata
+    //Generate Tarti metadata
     const tartiMetaData = {
-        "name": "",
-        "description": "",
+        "name": beatInfo["title"],
+        "description": "test description",
         "image": "",
         "animation_url": "", //this will store the mp3? Would be cool if we have a video instead. We kind of do rn.... Ifwe put a video here then the song would go into a custom field or an attribute?
-        "background_color": "",
-        "attributes": metaAttributes
-    }
-
-    const traitio = require("../traithttpclient");
-
-    try {
-        //generate beat package from MakeBeat which includes most of what we need from Trait API
-        //@todo consider breaking out the titling, videos, images etc from the actual beat making.
-        const beatInfo = (await traitio.promptBot("MakeBeat", tartistMetadata));
-        if ((typeof beatInfo !== 'object') || !beatInfo["title"]) {
-            throw new Error("Invalid response from Trait AI when trying to make beat: " + beatInfo);
-        }
-        tartiMetaData.name = beatInfo["title"];
-        const mp3PathOnBot = beatInfo["mp3"];
-        const tracksImagePathOnBot = beatInfo["png"];
-        tartiMetaData.image = "ipfs://" + (await traitio.promptBot("PinFilesToIpfs", tartistMetadata, { "Files": tracksImagePathOnBot }))[0]; //TraitHttpIO will return an IPFS CID
-        tartiMetaData.animation_url = "ipfs://" + (await traitio.promptBot("PinFilesToIpfs", tartistMetadata, { "Files": mp3PathOnBot }))[0]; //TraitHttpIO will return an IPFS CID
-        tartiMetaData.external_url = `http//tartiart.com/tarti/${tokenId}`;
-
-        //generate description
-        tartiMetaData.description = "test description";  //(await traitio.promptBot("GenerateSongDescription", tartistMetadata))[0];
-    } catch (error) {
-
-        console.log(error);
-        throw error;
-    }
-
-    //generate bots fav bg color
-    tartiMetaData.background_color = "FFFFFF";
-
-    try {
-        //upload metadata to IPFS usaing PInata
-        const pinataSDK = require('@pinata/sdk');
-        const pinata = new pinataSDK({ pinataJWTKey: process.env["PINATA_API_JWT"] });
-        const authResult = await pinata.testAuthentication();
-        //if authResult checkauth
-        const pinResponse = await pinata.pinJSONToIPFS(tartiMetaData, {
-            pinataMetadata: {
-                name: `${tartiMetaData.name.replace(/[^\x00-\x7F]|\s/g, "-")}-metadata.json`
+        "background_color": "FFFFFF",
+        "attributes": [
+            {
+                "trait_type": "birthday",
+                "display_type": "date",
+                "value": Math.floor(Date.now() / 1000)
             }
-        });
-        const metaDataFileHash = pinResponse.IpfsHash;
+        ]
+    }
+    tartiMetaData.image = "ipfs://" + (await traitio.promptBot("PinFilesToIpfs", tartistMetadata, { "Files": beatInfo["png"] }))[0]; //TraitHttpIO will return an IPFS CID
+    tartiMetaData.animation_url = "ipfs://" + (await traitio.promptBot("PinFilesToIpfs", tartistMetadata, { "Files": beatInfo["mp3"] }))[0]; //TraitHttpIO will return an IPFS CID
+    tartiMetaData.external_url = `http//tartiart.com/tarti/${tokenId}`;
 
-        //update the tokenuri on ethereum
-        tartistContract.methods.setCreated(tokenId, nft.web3.utils.fromAscii(metaDataFileHash), true).send({ from: nft.web3.eth.accounts.wallet[0].address });
-        context.log(`New tarti metadata hash: ${metaDataFileHash}`);
-    } catch (error) {
-        context.log(error);
-        throw error;
+    //Pin Tarti metadata to IPFS usaing Pinata
+    const pinataSDK = require('@pinata/sdk');
+    const pinata = new pinataSDK({ pinataJWTKey: process.env["PINATA_API_JWT"] });
+    const authResult = await pinata.testAuthentication();
+    const pinResponse = await pinata.pinJSONToIPFS(tartiMetaData, {
+        pinataMetadata: {
+            name: `${tartiMetaData.name.replace(/[^\x00-\x7F]|\s/g, "-")}-metadata.json`
+        }
+    });
+    const metaDataFileHash = pinResponse.IpfsHash;
+
+    //Update the Tarti's TokenURI to be that of the new Metadata
+    tartistContract.methods.setCreated(tokenId, nft.web3.utils.fromAscii(metaDataFileHash), true).send({ from: nft.web3.eth.accounts.wallet[0].address });
+
+    context.log(`Tarti ${tokenId} created, metadata hash: ${metaDataFileHash}`);
+
+    async function downloadFileToMemory(url) {
+        const axios = require('axios');
+        const response = await axios.get(url);
+        return response.data;
     }
 
-    context.log('JavaScript ServiceBus queue trigger function processed message', tartiSbMsg);
+    function convertIpfsToWeb2GatewayUri(uri) {
+        if (uri.substr(0, 7) == "ipfs://") {
+            uri = `${process.env.IPFS_GATEWAY}/${uri.substr(7)}`;
+        }
+        return uri;
+    }
 };
-
-async function downloadFileToMemory(url) {
-    const axios = require('axios');
-    const response = await axios.get(url);
-    return response.data;
-}
-
-function convertIpfsToWeb2GatewayUri(uri) {
-    if (uri.substr(0, 7) == "ipfs://") {
-        uri = `${process.env.IPFS_GATEWAY}/${uri.substr(7)}`;
-    }
-    return uri;
-}
